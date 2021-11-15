60751f35aed9a806be1824afdba43b9e25843530
Kevin A <12297645+Kevin-Abd@users.noreply.github.com>
2021-11-14 19:50:06 -0600

    formatting

--- step1_choose_center.py
+++ step1_choose_center.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-import logging
 from plot import *
-from cluster import *
 from utils import load_paperdata, convert_to_matrix
 
-def plot(data, auto_select_dc = False):
+
+def plot(data, auto_select_dc=False):
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
     distances_dict, max_dis, min_dis, max_id = load_paperdata(data)
     distances_matrix = convert_to_matrix(distances_dict, max_id)
     dpcluster = DensityPeakCluster()
-    rho, rc = dpcluster.local_density(distances_matrix, max_dis, min_dis, max_id, auto_select_dc = auto_select_dc)
+    rho, rc = dpcluster.local_density(distances_matrix, max_dis, min_dis, max_id, auto_select_dc=auto_select_dc)
     delta, nneigh = min_distance(max_id, max_dis, distances_matrix, rho)
-    plot_rho_delta(rho, delta)   #plot to choose the threthold
+    plot_rho_delta(rho, delta)  # plot to choose the threshold
+
 
 if __name__ == '__main__':
-    #plot('./data/data_in_paper/example_distances.dat')
-    #plot('./data/data_others/spiral_distance.dat')
-    #plot('./data/data_others/aggregation_distance.dat')
-    #plot('./data/data_others/flame_distance.dat')
+    # plot('./data/data_in_paper/example_distances.dat')
+    # plot('./data/data_others/spiral_distance.dat')
+    # plot('./data/data_others/aggregation_distance.dat')
+    # plot('./data/data_others/flame_distance.dat')
     plot('./data/data_others/jain_distance.dat')
--- step2_cluster.py
+++ step2_cluster.py
@@ -1,32 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-import logging
 from plot import *
-from cluster import *
 from utils import load_paperdata, convert_to_matrix
 
 
-def plot(data, density_threshold, distance_threshold, auto_select_dc = False):
-	logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+def plot(data, density_threshold, distance_threshold, auto_select_dc=False):
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-	distances_dict, max_dis, min_dis, max_id = load_paperdata(data)
-	distances_matrix = convert_to_matrix(distances_dict, max_id)
+    distances_dict, max_dis, min_dis, max_id = load_paperdata(data)
+    distances_matrix = convert_to_matrix(distances_dict, max_id)
 
-	dpcluster = DensityPeakCluster()
-	rho, delta, nneigh, cluster, ccenter = dpcluster.cluster(distances_matrix, max_dis, min_dis, max_id, density_threshold, distance_threshold, auto_select_dc = auto_select_dc)
-	logger.info(str(len(ccenter)) + ' center as below')
-	unique_centers = np.unique(ccenter)
-	for idx, center in np.ndenumerate(unique_centers):
-		logger.info('%d %d %f %f' % (idx[0], center, rho[center+1], delta[center+1]))
-	plot_rho_delta(rho, delta)   #plot to choose the threthold
-	plot_rhodelta_rho(rho,delta)
-	plot_cluster(dpcluster)
+    dpcluster = DensityPeakCluster()
+    rho, delta, nneigh, cluster, ccenter = dpcluster.cluster(distances_matrix, max_dis, min_dis, max_id,
+                                                             density_threshold, distance_threshold,
+                                                             auto_select_dc=auto_select_dc)
+    logger.info(str(len(ccenter)) + ' center as below')
+    unique_centers = np.unique(ccenter)
+    for idx, center in np.ndenumerate(unique_centers):
+        logger.info('%d %d %f %f' % (idx[0], center, rho[center + 1], delta[center + 1]))
+    plot_rho_delta(rho, delta)  # plot to choose the threshold
+    plot_rhodelta_rho(rho, delta)
+    plot_cluster(dpcluster)
 
 
 if __name__ == '__main__':
-	plot('./data/data_in_paper/example_distances.dat', 20, 0.1,False)
-	#plot('./data/data_others/spiral_distance.dat',8,5,False)
-	#plot('./data/data_others/aggregation_distance.dat',15,4.5,False)
-	#plot('./data/data_others/flame_distance.dat',4,7,False)
-	#plot('./data/data_others/jain_distance.dat',12,10,False)
+    plot('./data/data_in_paper/example_distances.dat', 20, 0.1, False)
+    # plot('./data/data_others/spiral_distance.dat',8,5,False)
+    # plot('./data/data_others/aggregation_distance.dat',15,4.5,False)
+    # plot('./data/data_others/flame_distance.dat',4,7,False)
+    # plot('./data/data_others/jain_distance.dat',12,10,False)
