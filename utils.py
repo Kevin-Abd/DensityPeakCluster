c9222fb2d94e7f888078d6ebb76ec94d8941f3af
Kevin A <12297645+Kevin-Abd@users.noreply.github.com>
2021-11-14 18:56:50 -0600

    add convert_to_matrix

--- utils.py
+++ utils.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-import sys
-import math
 import logging
+import sys
+
 import numpy as np
 
 logger = logging.getLogger("dpc_cluster")
+
 
 def load_paperdata(distance_f):
     """
@@ -35,3 +36,29 @@ def load_paperdata(distance_f):
         distances[(i, i)] = 0.0
     logger.info("PROGRESS: load end")
     return distances, max_dis, min_dis, max_id
+
+
+def convert_to_matrix(distances_dict: dict, max_id: int):
+    """
+    convert distances dict to distances matrix
+
+    Args:
+        distances_dict: distances dict
+        max_id:  max continues id equal to size
+
+    Returns:
+        distances matrix
+    """
+    logger.info("PROGRESS: convert data")
+    distances = np.zeros((max_id, max_id), float)
+
+    # note that that the distances_dict is one based while the matrix is zero based
+    for i in range(max_id):
+        for j in range(max_id):
+            if i == j:
+                distances[i, j] = 0
+            else:
+                distances[i, j] = distances_dict[(i + 1), (j + 1)]
+            distances[j, i] = distances[i, j]
+    logger.info("PROGRESS: convert end")
+    return distances
